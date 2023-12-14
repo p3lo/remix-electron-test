e3c93b7c8647bcc511ae9ca2b86cdcaa514a778a
p3lo <mysiacik@mysiacik.com>
2023-12-14 15:14:36 +0100

    Update dependencies and fix code formatting

---
+++ app/lib/functions.ts
@@ -0,0 +1,69 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+type NestedObject = {
+  [key: string]: any;
+};
+
+export async function openConfigFiles() {
+  const filePathConfig = path.join(__dirname, '..', 'config', 'config.json');
+  const fileContentsConfig = await fs.readFile(filePathConfig, 'utf8');
+  const config = JSON.parse(fileContentsConfig);
+  const filePathFolder = path.join(__dirname, '..', 'config', 'FOLDER.json');
+  const fileContentsFolder = await fs.readFile(filePathFolder, 'utf8');
+  const folder = JSON.parse(fileContentsFolder);
+  const filePathJob = path.join(__dirname, '..', 'config', 'JOB.json');
+  const fileContentsJob = await fs.readFile(filePathJob, 'utf8');
+  const job = JSON.parse(fileContentsJob);
+  const filePathOn = path.join(__dirname, '..', 'config', 'ON.json');
+  const fileContentsOn = await fs.readFile(filePathOn, 'utf8');
+  const on = JSON.parse(fileContentsOn);
+  const filePathQuantitative = path.join(__dirname, '..', 'config', 'QUANTITATIVE.json');
+  const fileContentsQuantitative = await fs.readFile(filePathQuantitative, 'utf8');
+  const quantitative = JSON.parse(fileContentsQuantitative);
+  const filePathVariable = path.join(__dirname, '..', 'config', 'VARIABLE.json');
+  const fileContentsVariable = await fs.readFile(filePathVariable, 'utf8');
+  const variable = JSON.parse(fileContentsVariable);
+  assignAttributes(config, 'FOLDER', folder);
+  assignAttributes(config, 'JOB', job);
+  assignAttributes(config, 'ON', on);
+  assignAttributes(config, 'QUANTITATIVE', quantitative);
+  assignAttributes(config, 'VARIABLE', variable);
+  return config;
+}
+
+function assignAttributes(obj: NestedObject, keyToInsert: string, insertJsonConfig: NestedObject) {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        if (key === keyToInsert) {
+          // Merge the insertJsonConfig into the existing object
+          obj[key] = { ...obj[key], ...insertJsonConfig };
+        } else {
+          // Recursively call for nested objects
+          assignAttributes(obj[key], keyToInsert, insertJsonConfig);
+        }
+      }
+    }
+  }
+}
+
+function formatXml(xml: string): string {
+  let formatted = '';
+  let indent = '';
+  const tab = '  '; // Define tab as two spaces for indentation
+
+  xml.split(/>\s*</).forEach((element) => {
+    if (element.match(/^\/\w/)) {
+      indent = indent.substring(tab.length); // Decrease indentation
+    }
+
+    formatted += indent + '<' + element + '>\r\n';
+
+    if (element.match(/^<?\w[^>]*[^/]$/)) {
+      indent += tab; // Increase indentation
+    }
+  });
+
+  return formatted.substring(1, formatted.length - 3);
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,21 @@
-import { type ActionFunctionArgs } from '@remix-run/node';
+import { LoaderFunctionArgs, type ActionFunctionArgs } from '@remix-run/node';
 import { Form, useLoaderData } from '@remix-run/react';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
-import electron from '~/electron.server';
 import * as XLSX from 'xlsx';
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
 import { Button } from '~/components/ui/button';
+import { openConfigFiles } from '~/lib/functions';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '~/components/ui/select';
+import { Separator } from '~/components/ui/separator';
 
 type SheetData = { [key: string]: any };
 
-export function loader() {
+export async function loader({}: LoaderFunctionArgs) {
+  // fetch external file from data directory
+  const getConfig = await openConfigFiles();
   return {
-    userDataPath: electron.app.getPath('userData'),
+    getConfig: getConfig,
   };
 }
 
@@ -25,7 +29,22 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 export default function Index() {
   const dataFromLoader = useLoaderData<typeof loader>();
   const [data, setData] = useState<SheetData[]>([]);
-  console.log(data);
+  const [interval, setInterval] = useState<string[]>([]);
+  const [selectCyclicValues, setSelectCyclicValues] = useState<
+    Array<{ interval: string; TYPE: string; VALUES: string; MINORHOURS: string; TIMEZONE: string }>
+  >([]);
+  console.log(selectCyclicValues);
+  useEffect(() => {
+    if (data.length > 0 && dataFromLoader.getConfig) {
+      let intervals: string[] = [];
+      data.forEach(async (job) => {
+        if (job.Z && !intervals.includes(job.Z)) {
+          intervals.push(job.Z);
+        }
+      });
+      setInterval(intervals);
+    }
+  }, [data]);
 
   const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -46,9 +65,9 @@ export default function Index() {
   };
 
   return (
-    <main className="flex flex-col gap-y-5">
+    <main className="flex flex-col gap-y-5 w-full">
       <h1 className="p-3 font-bold">Welcome to Remix</h1>
-      <p>User data path: {dataFromLoader.userDataPath}</p>
+      {/* {dataFromLoader.getConfig && <p>User data path: {JSON.stringify(dataFromLoader.getConfig)}</p>} */}
       <div className="flex gap-x-5 items-end justify-center">
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="excel">Excel template</Label>
@@ -61,12 +80,169 @@ export default function Index() {
         </div>
       </div>
       {data.length > 0 && (
-        <p className="flex gap-x-3 justify-center">
-          Loaded jobs: <pre>{data.length}</pre>
-        </p>
+        <div className="w-full flex flex-col gap-y-2 ">
+          <p className="flex gap-x-3 justify-center text-sm">Loaded jobs: {data.length}</p>
+          <form className="flex flex-col gap-y-2 justify-center items-center">
+            <div className="grid w-full items-center gap-1.5 max-w-sm">
+              <Label htmlFor="nodeid">NodeId</Label>
+              <Input id="nodeid" type="text" placeholder="Set NODEID" />
+            </div>
+            <Separator className="my-4" />
+            {interval?.map((interval, index) => {
+              const currentInterval = selectCyclicValues.find((item) => item.interval === interval) || {
+                interval: { interval },
+                TYPE: 'cyclic',
+                VALUES: '',
+                MINORHOURS: 'minutes',
+                TIMEZONE: 'cst',
+              };
+              return (
+                <div key={index} className="grid w-full justify-center items-center gap-1.5">
+                  <Label className="text-xs" htmlFor={index.toString()}>
+                    Correct cyclic intervals
+                  </Label>
+                  <p className="text-xs">{interval}</p>
+                  <div className="flex gap-x-1">
+                    <Select
+                      name="cyclic"
+                      defaultValue={currentInterval.TYPE}
+                      onValueChange={(value) => {
+                        const newSelectValues = [...selectCyclicValues];
+                        const existingIntervalIndex = newSelectValues.findIndex((item) => item.interval === interval);
+                        if (existingIntervalIndex > -1) {
+                          if (value === 'sequence') {
+                            newSelectValues[existingIntervalIndex].TIMEZONE = currentInterval.TIMEZONE;
+                          } else {
+                            newSelectValues[existingIntervalIndex].MINORHOURS = currentInterval.MINORHOURS;
+                          }
+                          newSelectValues[existingIntervalIndex].TYPE = value;
+                        } else {
+                          if (value === 'sequence') {
+                            newSelectValues.push({
+                              interval,
+                              TYPE: value,
+                              VALUES: currentInterval.VALUES,
+                              MINORHOURS: currentInterval.MINORHOURS,
+                              TIMEZONE: currentInterval.TIMEZONE,
+                            });
+                          } else {
+                            newSelectValues.push({
+                              interval,
+                              TYPE: value,
+                              VALUES: currentInterval.VALUES,
+                              MINORHOURS: currentInterval.MINORHOURS,
+                              TIMEZONE: currentInterval.TIMEZONE,
+                            });
+                          }
+                        }
+                        setSelectCyclicValues(newSelectValues);
+                      }}
+                    >
+                      <SelectTrigger className="w-[110px]">
+                        <SelectValue placeholder="Cyclic" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="cyclic">Cyclic</SelectItem>
+                        <SelectItem value="sequence">Sequence</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <Input
+                      id={index.toString()}
+                      type="text"
+                      placeholder={
+                        currentInterval.TYPE === 'cyclic' ? 'Set cyclic value' : 'Set sequence values delimited by ,'
+                      }
+                      name="corrected"
+                      className="w-[500px]"
+                      value={currentInterval.VALUES}
+                      onChange={(e) => {
+                        const newSelectValues = [...selectCyclicValues];
+                        const existingIntervalIndex = newSelectValues.findIndex((item) => item.interval === interval);
+                        if (existingIntervalIndex > -1) {
+                          newSelectValues[existingIntervalIndex].VALUES = e.target.value;
+                        } else {
+                          newSelectValues.push({
+                            interval,
+                            TYPE: currentInterval.TYPE,
+                            VALUES: e.target.value,
+                            MINORHOURS: currentInterval.MINORHOURS,
+                            TIMEZONE: currentInterval.TIMEZONE,
+                          });
+                        }
+                        setSelectCyclicValues(newSelectValues);
+                      }}
+                    />
+                    {currentInterval.TYPE === 'cyclic' ? (
+                      <Select
+                        name="cyclic-c"
+                        defaultValue={currentInterval.MINORHOURS}
+                        value={currentInterval.MINORHOURS}
+                        onValueChange={(value) => {
+                          const newSelectValues = [...selectCyclicValues];
+                          const existingIntervalIndex = newSelectValues.findIndex((item) => item.interval === interval);
+                          if (existingIntervalIndex > -1) {
+                            newSelectValues[existingIntervalIndex].MINORHOURS = value;
+                          } else {
+                            newSelectValues.push({
+                              interval,
+                              TYPE: currentInterval.TYPE,
+                              VALUES: currentInterval.VALUES,
+                              MINORHOURS: value,
+                              TIMEZONE: currentInterval.TIMEZONE,
+                            });
+                          }
+                          setSelectCyclicValues(newSelectValues);
+                        }}
+                      >
+                        <SelectTrigger className="w-[100px]">
+                          <SelectValue placeholder="minutes" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="minutes">Minutes</SelectItem>
+                          <SelectItem value="hours">Hours</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    ) : (
+                      <Select
+                        name="cyclic-t"
+                        value={currentInterval.TIMEZONE}
+                        defaultValue={currentInterval.TIMEZONE}
+                        onValueChange={(value) => {
+                          const newSelectValues = [...selectCyclicValues];
+                          const existingIntervalIndex = newSelectValues.findIndex((item) => item.interval === interval);
+                          if (existingIntervalIndex > -1) {
+                            newSelectValues[existingIntervalIndex].TIMEZONE = value;
+                          } else {
+                            newSelectValues.push({
+                              interval,
+                              TYPE: currentInterval.TYPE,
+                              VALUES: currentInterval.VALUES,
+                              MINORHOURS: currentInterval.MINORHOURS,
+                              TIMEZONE: value,
+                            });
+                          }
+                          setSelectCyclicValues(newSelectValues);
+                        }}
+                      >
+                        <SelectTrigger className="w-[100px]">
+                          <SelectValue placeholder="timezone" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="cst">CST</SelectItem>
+                          <SelectItem value="est">EST</SelectItem>
+                          <SelectItem value="cet">CET</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          </form>
+        </div>
       )}
       <Form method="post">
-        <input hidden name="data" type="text" value={JSON.stringify(data)} />
+        <input hidden name="data" type="text" value={JSON.stringify(data)} readOnly />
         <Button type="submit">Submit</Button>
       </Form>
     </main>
